aa57dabd54081590e661eb143115593e410e742d
Smruti Ranjan Senapati <smruti.ranjan@rapyuta-robotics.com>
2020-08-27 18:26:58 +0530

    change listerner

--- talk/listener/listener.py
+++ talk/listener/listener.py
@@ -5,7 +5,7 @@ import os
 from std_msgs.msg import String
 
 def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.data)
+	rospy.loginfo(rospy.get_caller_id() + ' I heard the new %s', data.data)
 
 
 def listener():
